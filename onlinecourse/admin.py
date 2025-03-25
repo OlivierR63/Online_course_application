5beff123dc0e0269c2640f6f4d789c979ef9ab9d
OlivierR63 <olivier.rochat@gmail.com>
2025-03-25 11:42:45 -0400

    Final commit

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -4,29 +4,23 @@ from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submi
 class QuestionInline(admin.StackedInline):
     model = Question
     extra = 2
-
 class ChoiceInline(admin.StackedInline):
     model = Choice
     extra = 2
-
 class SubmissionInline(admin.StackedInline):
     model = Submission
     extra = 2
-
 class LessonInline(admin.StackedInline):
     model = Lesson
     extra = 2
-
 class CourseAdmin(admin.ModelAdmin):
     inlines = [LessonInline]
     list_display = ('name', 'pub_date')
     list_filter = ['pub_date']
     search_fields = ['name', 'description']
-
 class QuestionAdmin(admin.ModelAdmin):
     inlines=[ChoiceInline]
     list_display=['content']
-
 class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
